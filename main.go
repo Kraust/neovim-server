b8fa09530596f54daadb9615b83f72614f9ff953
Kraust <secretdragoon@gmail.com>
2025-08-30 10:05:48 -0400

    Pass in address.

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"flag"
 	"github.com/Kraust/nvim-server/server"
 	"log"
 )
 
+var (
+	f_address = flag.String("address", "127.0.0.1:9998", "Specifies the address to bind the server to.")
+)
+
 func main() {
-	err := server.Serve("127.0.0.1:9998")
+	flag.Parse()
+	err := server.Serve(*f_address)
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
